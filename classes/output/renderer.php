a260af5289f343fbe6224cc7d71367363c647c7b
fabmen <menard.fabrice@gmail.com>
2015-07-15 14:07:09 +0200

    change navbar4

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -100,4 +100,77 @@ class renderer extends theme_bootstrapbase_core_renderer {
         $data = $page->export_for_template($this);
         return parent::render_from_template('theme_ugabase/wrapper_layout', $data);
     }
+    public function courses_menu() {
+        global $CFG;
+        $coursesmenu = new custom_menu('', current_language());
+        return $this->render_courses_menu($coursesmenu);
+    }
+
+    protected function render_courses_menu(custom_menu $menu){
+        if (isloggedin() && !isguestuser() ) {
+            $branchtitle = get_string('mycourses');
+            $branchlabel = '<i class="fa fa-briefcase"></i> '.$branchtitle;
+            $branchurl   = new moodle_url('/my/index.php');
+            $branchsort  = 10000;
+
+            $branch = $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);
+            if ($courses = enrol_get_my_courses(NULL, 'fullname ASC')) {
+                foreach ($courses as $course) {
+                    if ($course->visible){
+                        $branch->add(format_string($course->fullname), new moodle_url('/course/view.php?id='.$course->id), format_string($course->shortname));
+                    }
+                }
+            } 
+            $branchtitle = get_string('showallcourses');
+            $branchlabel = '<i class="fa fa-sitemap"></i> '.$branchtitle;
+            $branchurl   = new moodle_url('/course/index.php');
+            $branchsort  = 10000;
+            $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);
+
+        $content = '<ul class="nav navbar-nav">';
+        foreach ($menu->get_children() as $item) {
+            $content .= $this->render_custom_menu_item($item, 1);
+        }
+    
+        return $content.'</ul>';
+        }
+    }
+
+/*
+     * This renders the bootstrap top menu.
+     *
+     * This renderer is needed to enable the Bootstrap style navigation.
+     */
+    protected function render_custom_menu(custom_menu $menu) {
+        global $CFG;
+
+        // TODO: eliminate this duplicated logic, it belongs in core, not
+        // here. See MDL-39565.
+        $addlangmenu = true;
+        $langs = get_string_manager()->get_list_of_translations();
+        if (count($langs) < 2
+            or empty($CFG->langmenu)
+            or ($this->page->course != SITEID and !empty($this->page->course->lang))) {
+            $addlangmenu = false;
+        }
+
+        if (!$menu->has_children() && $addlangmenu === false) {
+            return '';
+        }
+
+        if ($addlangmenu) {
+            $language = $menu->add("<i class='fa fa-flag'></i> ".get_string('language'), new moodle_url('#'), get_string('language'), 10000);
+            foreach ($langs as $langtype => $langname) {
+                $language->add($langname, new moodle_url($this->page->url, array('lang' => $langtype)), $langname);
+            }
+        }
+
+        $content = '<ul class="nav">';
+        foreach ($menu->get_children() as $item) {
+            $content .= $this->render_custom_menu_item($item, 1);
+        }
+
+        return $content.'</ul>';
+    }
+
 }
